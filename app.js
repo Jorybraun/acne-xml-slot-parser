dfe20b44ffb2a0efbd8e4b3c4c2cf619f2e01038
Jory <braunjory@gmail.com>
2018-09-26 11:07:27 +0200

    adds testing and refactors

--- app.js
+++ app.js
@@ -3,16 +3,36 @@ var fs = require('fs'),
  
 var parser = new xml2js.Parser();
 
-fs.readFile('./home.xml', (err, file) => {
-    console.log(file)
-    parser.parseString(file, (err, json) => {
-        const slots = Object.entries(json['slot-configurations']['slot-configuration'])
-        
-        console.log()
-
-        for (const slot of slots) {
-            console.log(slot[1].content)
+const checkForContentId = (content, id) => {
+    if (content instanceof Array) {
+        const contentAssets = content[0]['content-assets']
+        // checkout the content assets tag for corresponding element with ID
+        if (contentAssets && contentAssets.length) {
+            contentAssets[0]['content-asset'].forEach((contentId) => {
+                return contentId.$['content-id'].includes(id)
+            })
         }
+    }
+}
 
+// create new xml file from json
+
+const parseFile = (file) => {
+    return fs.readFile(file, (err, file) => {
+        return parser.parseString(file, (err, json) => {
+            const slots = json['slot-configurations']['slot-configuration']
+
+            for (const slot of slots) {
+                const content = slot.content
+                // check slot for site specific id this doesnt belong
+                if (checkForContentId(content, 'acne_se')) {
+                    // delete the slot
+                    delete slot
+                }
+            }
+            return slots
+        })
     })
-})
+}
+
+modules.exports = parseFile
