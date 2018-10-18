5110194fdffa301e8ea4ba9ebf1baf5db66bb454
Jory <braunjory@gmail.com>
2018-10-18 11:50:01 +0200

    DS-2174: adds cb to filter recursive search

--- src/recursiveSearch.ts
+++ src/recursiveSearch.ts
@@ -1,15 +1,19 @@
 
-export const recursiveSearch = (obj: { [key: string]: any }, key: string, value: any): boolean => {
+export const recursiveSearch = (obj: { [key: string]: any }, key: string, value: any, cb: (obj: {}, value: any) => boolean): boolean => {
     let found = false;
-    if (obj[key] === value) { return true }
+    // check the value again a callback to make function more versatile
+    if (cb(obj[key], value)) { return true }
 
     for (const x in obj) {
         let result = false;
+        // if its an object search deeper
         if (typeof obj[x] == 'object') {
-            result = recursiveSearch(obj[x], key, value);
+            // recursively search again
+            result = recursiveSearch(obj[x], key, value, cb);
         }
 
         if (result === true) {
+            // if we find the value return early
             found = true;
         }
     }
